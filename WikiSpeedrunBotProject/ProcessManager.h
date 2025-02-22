fcf7aca3d5a39243454a1f76ef6c36d862e1a6d3
Liron <vaismanliron@gmail.com>
2025-02-22 14:46:18 +0200

    shalom

--- WikiSpeedrunBotProject/ProcessManager.h
+++ WikiSpeedrunBotProject/ProcessManager.h
@@ -1,5 +1,10 @@
 #pragma once
+
+#define MAX_SEARCH_DEPTH 10
+
 class ProcessManager
 {
+private:
+	int _maxThreads;
 };
 
--- WikiSpeedrunBotProject/WikiStructs.h
+++ WikiSpeedrunBotProject/WikiStructs.h
@@ -1,1 +1,7 @@
 #pragma once
+#include <iostream>
+#include <vector>
+typedef struct WikiData
+{
+	std::vector<std::string> pathsToEndPage;
+}WikiData;
